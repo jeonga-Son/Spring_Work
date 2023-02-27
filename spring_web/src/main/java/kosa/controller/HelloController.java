210cd07538c4a13d195a45960707ba68019127c9
jeonga-Son <jjeong0106@gmail.com>
2023-02-27 14:22:02 +0900

    Spring MVC test3

--- spring_web/src/main/java/kosa/controller/HelloController.java
+++ spring_web/src/main/java/kosa/controller/HelloController.java
@@ -2,6 +2,7 @@ package kosa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -20,6 +21,13 @@ public class HelloController {
 		mav.setViewName("hello");
 	
 		return mav;
+	}
+	
+	@RequestMapping("/hello2.do")
+	public String hello2(Model model) {
+		model.addAttribute("message", "ÆÄÀÌÆÃ");
+		
+		return "hello";
 	}
 	
 }
