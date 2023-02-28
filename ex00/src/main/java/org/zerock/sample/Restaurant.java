cc9dcc6ec6ccdaec15dcdac8fbe7ee6ec25cb7b7
jeonga-Son <jjeong0106@gmail.com>
2023-02-28 15:37:18 +0900

    [style] jdbc 라이브러리 추가

--- ex00/src/main/java/org/zerock/sample/Restaurant.java
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -3,10 +3,14 @@ package org.zerock.sample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Component
 @Data
+@AllArgsConstructor
+@NoArgsConstructor // default 생성자
 public class Restaurant {
 	@Autowired
 	private Chef chef;
